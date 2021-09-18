b43150867eff02d3010c68a3696bc2b94482d2c8
Vikram Singh <mr.vikram004@gmail.com>
2021-09-18 21:45:05 +0530

    Properties access

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import { HelloJSX } from './jsxVSjs/Hello';
 import { HelloJS } from './jsxVSjs/Hello';
+import { GreetFunctionProperties } from './functionProperties/PropertiesComponent';
+import { GreetFunctionMultiProperties } from './functionProperties/PropertiesComponent';
+import { GreetClassProperties } from './functionProperties/PropertiesComponent'
+import { GreetClassMultiProperties } from './functionProperties/PropertiesComponent'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <HelloJSX />
-      <HelloJS />
+    {/*   
+        //  JSx VS JS  
+          <HelloJSX />
+          <HelloJS />
+       */
+      }
+
+      {/*Properties Explanation*/}
+
+      <h1>Functional Properties Compnent</h1>
+      <GreetFunctionProperties name = "Bruce" role='Batman'/>
+      <GreetFunctionProperties name = "Clarke" role='Superman'/>
+     
+      <GreetFunctionMultiProperties name = "Diana" role='WonderWoman'>
+        <p>This is Children props</p>
+      </GreetFunctionMultiProperties>
+      <GreetFunctionMultiProperties name = "adam" role='Black Adam'/>
+
+      <h1>Class Properties Compnent</h1>
+      <GreetClassProperties name = "Peter" role='Spiderman'/>
+      <GreetClassProperties name = "Tony" role='IronMan'/>
+
+      <GreetClassMultiProperties name = "Tchalla" role='Black Panther'>
+        <button>Upload</button>
+      </GreetClassMultiProperties>
+
+      <GreetClassMultiProperties name = "Bruce" role='Hulk'/>
     </div>
   );
 }
---
+++ src/functionProperties/PropertiesComponent.js
@@ -0,0 +1,56 @@
+import react from "react";
+import { Component } from "react";
+
+export const GreetFunctionProperties = (properties) => {
+    console.log(properties)
+    return <h3> Hello {properties.name} aka {properties.role} </h3>
+}
+
+/*
+    1)Properties is used to dynamically fetch the elements from the included component.
+    2)You can give any name to the properties aka props, properties, vikram or anyName
+*/
+
+export class GreetClassProperties extends Component {
+    render() {
+        return <h3> Hello {this.props.name} aka {this.props.role} </h3>
+    }
+}
+
+/*
+    1)To access the properties, from the class component, you need to call it as 'this.props',
+        where, this refers to current class, props is internally defined as member object
+*/
+
+export const GreetFunctionMultiProperties = (properties) => {
+    return (
+        <div>
+            <h3> Hello {properties.name} aka {properties.role} </h3>
+            {properties.children}
+        </div>
+    )
+}
+
+/*
+    1)Here we have returend the HTML content inside brackets because, we need to render multiple
+        elements, for which we also required the div tag.
+            As JSX can render only one elements from a function, to return or render multiple elements
+        it need to be enclosed in a div tag, else it will report error
+    
+    2) Here 'properties.children' will render any enclosed elements, if it's present
+*/
+
+export class GreetClassMultiProperties extends Component {
+    render() {
+        return (
+        <div>
+            <h3> Hello {this.props.name} aka {this.props.role} </h3>
+            {this.props.children}
+        </div>
+        )
+    }
+}
+
+/*
+    same as GreetFunctionMultiProperties except it's a class
+*/
