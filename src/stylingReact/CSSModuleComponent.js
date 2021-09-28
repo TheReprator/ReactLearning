1ce1461e2d9da2c02a64ce7a911ef06cc85ff1f4
Vikram Singh <mr.vikram004@gmail.com>
2021-09-28 20:43:12 +0530

    css Module component method 3

---
+++ src/stylingReact/CSSModuleComponent.js
@@ -0,0 +1,11 @@
+/*
+    CSS Module is availabe from reach 2.0.0, below it's not available. So, make sure 'react-scripts'
+    is more than 2.0.0 in package.json
+
+    In order to use any css module, file name should have extension of fileName.module.css like the file
+    'cssModule.module.css' in src/stylingReact/CSS folder
+
+    CSS Module provide a local scope, which means where ever we have to use CSS module, we have to 
+    import it where as in case of reguar css, once defined in parent component, there is no need to
+    import it in child component, for further refernce kindly look into video no. 'ReactJS Tutorial - 20 - Styling and CSS Basics(720P_HD)'
+*/ 
