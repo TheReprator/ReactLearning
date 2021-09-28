21e30166f3907ca374f77886865c1600443b6b8d
Vikram Singh <mr.vikram004@gmail.com>
2021-09-28 18:33:49 +0530

    multipe css on div tag with css Stylesheet as M1

--- src/stylingReact/CSSStylingComponent.js
+++ src/stylingReact/CSSStylingComponent.js
@@ -19,10 +19,30 @@ function CSSStyleSheet() {
 
 export default CSSStyleSheet
 
-/*
-    Here we had used method 1 to apply CSS on body,
 
-    At line no. 10, we had imported the style from myStyle.css
-    At line no. 14, we had applied the css class(named 'primary') with class name for particular 
-    div tag
+export function CSSStyleSheetProps(props) {
+    let cssClassName = props.name ? 'primary' : ''
+    return (
+        // <div className = {cssClassName}>
+        <div className = {`${cssClassName} font-xl`} >
+            <h2> CSS StyleSheet Primary</h2>
+        </div>
+    )
+}
+
+/*
+    1)
+        Here we had used method 1 to apply CSS on body,
+
+        At line no. 10, we had imported the style from myStyle.css
+        At line no. 14, we had applied the css class(named 'primary') with class name for particular 
+        div tag
+
+    2)
+        We can also dynamically place the css based on some conditions like we had done at line no. 24,
+        At line no. 27, we had applied multiple css class, since one is condition css(primary) 
+        and other is 'font-xl'
+
+        To apply just one, please uncomment line 26, and comment 27.
+
 */ 
